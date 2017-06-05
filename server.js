9540da49096c9fc7565b6c21ea9348996bfdc44a
BOlga <mathlace@mail.ru>
2017-06-05 17:56:23 +0400

    +eslint

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-﻿var express = require('express');
-var https = require('https');
-var cookieParser = require('cookie-parser');
-var constants = require('./constants.js');
-var methods = require('./methods.js');
-var vk = require('./vk.js');
-var hbs = require('hbs');
+﻿const express = require('express');
+const cookieParser = require('cookie-parser');
+const constants = require('./constants.js');
+const methods = require('./methods.js');
+const vk = require('./vk.js');
+const hbs = require('hbs');
 
 hbs.registerHelper('dateFormat', require('handlebars-dateformat'));
 
-var app = express();
-app.set('views', __dirname + '/views');
-app.set("view engine", "hbs");
-app.use(express.static(__dirname + '/public'));
+const app = express();
+app.set('views', __dirname +'/views'); // eslint no-path-concat: 2
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public')); // eslint no-path-concat: 2
 app.use(cookieParser());
 
 
 app.use(function (request, response, next) {
-    var cookie = request.cookies.VKUserData;
+    const cookie = request.cookies.VKUserData;
     if (cookie === undefined) {
-        if (request.path == constants.authUrl) {
+        if (request.path === constants.authUrl) {
             next();
         }
         else {
@@ -26,7 +25,7 @@ app.use(function (request, response, next) {
         }
     }
     else {
-        if (request.path == constants.showUrl) {
+        if (request.path === constants.showUrl) {
             next();
         }
         else {
@@ -37,11 +36,11 @@ app.use(function (request, response, next) {
 
 app.get(constants.authUrl, function (request, response) {
     if (request.query.code) {
-        var code = request.query.code;
+        let code = request.query.code;
         vk.getUserToken(code, methods.getFullPath(request), response);
     }
     else {
-        var url = vk.getVKAuthPath(methods.getFullPath(request));
+        let url = vk.getVKAuthPath(methods.getFullPath(request));
         response.render('auth', {
             vk_url: url,
             title: 'Авторизация'
@@ -50,7 +49,7 @@ app.get(constants.authUrl, function (request, response) {
 })
 
 app.get(constants.showUrl, function (request, response) {
-    var cookie = request.cookies.VKUserData;
+    let cookie = request.cookies.VKUserData;
     vk.getUserGroupData(cookie)
         .then(result => {
             if(result){
